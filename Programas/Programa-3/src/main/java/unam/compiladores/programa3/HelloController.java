98e3eb5d38163ab609bbd62e7cf8726aef16b90c
CrizJimz <c.chrisjim@gmail.com>
2026-02-21 13:49:38 -0600

    Programa 3 AFD que detecta un digito o letra

--- Programas/Programa-3/src/main/java/unam/compiladores/programa3/HelloController.java
+++ Programas/Programa-3/src/main/java/unam/compiladores/programa3/HelloController.java
@@ -1,14 +1,79 @@
 package unam.compiladores.programa3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+
+import java.io.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button BtArchivo;
+
+    @FXML
+    private TextArea TxtA_Archivo;
+
+    @FXML
+    void LeerArchivo(ActionEvent event) throws FileNotFoundException {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter ExtF = new FileChooser.ExtensionFilter("Archivos de texto", "*.txt");
+        fileChooser.getExtensionFilters().add(ExtF);
+        File archivo = fileChooser.showOpenDialog(null);
+        try (FileReader fr = new FileReader(archivo)){
+            BufferedReader br = new BufferedReader(fr);
+            StringBuilder sb = new StringBuilder();
+
+            String texto;
+            while ((texto=br.readLine()) != null) {
+                if (ValidarCaracteres(texto) != 3){
+                    sb.append(texto).append(": Invalido\n");
+                }else {
+                    sb.append(texto).append(": Valido\n");
+                }
+            }
+            TxtA_Archivo.setText(sb.toString());
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    int ValidarCaracteres(String Linea) throws IOException {
+
+        int Estado = 1;
+            for (char c : Linea.toCharArray()){
+
+                switch (Estado){
+                    case 1:
+                        if (Character.isLetter(c)){
+                            Estado = 3;
+                        } else if (Character.isDigit(c)) {
+                            Estado = 2;
+                        } else {
+                            throw new IllegalArgumentException("Argumento no valido:  " + c);
+                        }
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        if (Character.isLetter(c)) {
+                            Estado = 3;
+                        } else if (Character.isDigit(c)) {
+                            Estado = 3;
+                        } else {
+                            throw new IllegalArgumentException("Argumento no valido:  " + c);
+                        }
+
+                }
+
+            }
+        return Estado;
     }
 }
