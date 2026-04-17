63303e0d5898c49ebc150676218860a94f040557
CrizJimz <c.chrisjim@gmail.com>
2026-04-16 23:52:14 -0600

    Entregables parcial 1

--- Programas/Programa-3-V3/src/main/java/unam/compiladores/programa3v3/HelloApplication.java
+++ Programas/Programa-3-V3/src/main/java/unam/compiladores/programa3v3/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Programa 3 Tabla de transición");
         stage.setScene(scene);
         stage.show();
     }
--- Programas/Programa-3-V3/src/main/java/unam/compiladores/programa3v3/HelloController.java
+++ Programas/Programa-3-V3/src/main/java/unam/compiladores/programa3v3/HelloController.java
@@ -5,10 +5,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class HelloController {
+
+    private static final int [][] tablaTransicion=  {{2,1,0},{1,1,0},{2,2,1}};
 
     @FXML
     private Button BtArchivo;
@@ -23,6 +29,51 @@ public class HelloController {
         fileChooser.getExtensionFilters().add(ExtF);
         File archivo = fileChooser.showOpenDialog(null);
 
+        List<String> textos = Files.readAllLines(archivo.getPath());
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader (new FileReader(archivo))){
+            String texto;
+
+            while ((texto = br.readLine()) != null) {
+                if (ValidarPalabra(texto)){
+                    sb.append(texto).append(": Valido\n");
+                }else {
+                    sb.append(texto).append(": Invalido\n");
+                }
+            }
+            TxtA_Archivo.setText(sb.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    boolean ValidarPalabra (String linea){
+        int Estado = 0;
+        int Entrada;
+
+        do {
+            for (Character c : linea.toCharArray()) {
+                if (Character.isLetter(c)) {
+                    Entrada = 0;
+                } else if (Character.isDigit(c)) {
+                    Entrada = 1;
+                } else if (Character.isWhitespace(c)) {
+                    Entrada = 2;
+                    break;
+                } else {
+                    break;
+                }
+                Estado = tablaTransicion[Estado][Entrada];
+                if (Estado == 0) {
+                    break;
+                }
+            }
+
+        } while (Estado != 0);
+
+        return (Estado == 1);
     }
 
 
